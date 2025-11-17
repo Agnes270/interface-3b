1255553725374e62d9f4d1969adadbad99584f8f
Agnes270 <carvalho.cecilia@escolar.ifrn.edu.br>
2025-11-17 17:08:57 -0300

    01;08;07

--- interface 3bb/Assets/Scripts/Bala.cs
+++ interface 3bb/Assets/Scripts/Bala.cs
@@ -4,10 +4,15 @@ public class Bala : MonoBehaviour
 {
    
     void Start()
-    {
-        
-    }
+   {
+       [Serialize] private int dano = 1
+     [Serialize] private float velocidade = 15f;
+     private Renderer m_Renderer;
+     public void setDano(int dano)
 
+    {
+        this dano = dano;
+    }
    
     void Update()
     {
