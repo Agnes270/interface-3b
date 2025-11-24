f748a8711533aefb3f61d1b01dde19e4afd79d0f
Agnes270 <carvalho.cecilia@escolar.ifrn.edu.br>
2025-11-24 16:47:27 -0300

    minuto 15

--- interface 3bb/Assets/Scripts/Inimigo.cs
+++ interface 3bb/Assets/Scripts/Inimigo.cs
@@ -1,16 +1,77 @@
 using UnityEngine;
 
-public class Inimigo : MonoBehaviour
+public class Inimigo : Personagem
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   [SerializeField] private int dano = 1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public float raioDeVisao = 1;
+   public CircleCollider2D _visaoCollider2D;
+
+   [SerializeField] private Transform posicaoPlayer;
+
+   private SpriteRenderer spriteRenderer;
+   private Animator animator;
+
+   private bool andando = false;
+
+   public void setdano(int dano)
+
+   {
+      this.dano = dano;
+   }
+
+   public int getdano()
+   {
+      return this.dano;
+
+   }
+
+   void Start()
+   {
+      spriteRenderer = GetComponent<SpriteRenderer>();
+      animator = GetComponent<Animator>();
+
+      if (posicaoPlayer == null)
+      {
+         posicaoPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+      }
+
+      raioDeVisao = _visaoCollider2D.radius;
+   }
+
+   void Update()
+   {
+      andando = false;
+      if (getvidas() > 0)
+      {
+         spriteRenderer.flipX = false;
+
+      }
+
+      if (posicaoPlayer.position.x - transform.position.x < 0)
+      {
+         spriteRenderer.flipX = false;
+      }
+
+      if (posicaoPlayer.position.x - transform.position.x < 0)
+
+      {
+         spriteRenderer.flipX = true;
+      }
+
+      if (posicaoPlayer != null)
+      {
+       if(  Vector3.Distance(posicaoPlayer.position, transform.position) <= raioDeVisao)
+         {
+            Debug.Log("No raio de visao: " + posicaoPlayer.position);
+            transform.position = Vector3.MoveTowards(transform.position, posicaoPlayer.transform.position
+               , getVelocidade() * Time.deltaTime);
+
+            andando = true;
+         }
+
+      }
+   }
+
+
 }
